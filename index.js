06a5b9a672ff91529ee7c2b1b6c70bc559966fd2
mahiprivateuse <mahi.jayashankarmahesh@gmail.com>
2020-07-24 18:37:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const express = require('express');
-const app = express();
 const api = require("@what3words/api");
 
-api.setOptions({ key: "D6COMFQX" });
 
-app.get('/LatLongToW3W', function (req, res) {
-	console.log("Inside GET");
-	let responseData
-	api.convertTo3wa({lat:12.929430, lng: 77.547850})
-  .then(data =>{
-	  //res.end(data)
-	  console.log("this is it")
-	  res.send(data)
-  }
-	  )
+api.setOptions({ key: "D6COMFQX" });
+api.convertTo3wa({lat:12.929430, lng: 77.547850})
+  .then(data => console.log(data))
   .catch(function (error){
-	  console.log(error.toString());
+	  console.log(error.toString);
 	console.log("[code]",error.code);
 	console.log("[message]",error.message);
   });
-})
-  
-  
- var server = app.listen(8086, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
